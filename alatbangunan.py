5e4374a3e399858b2bc86e9e0b7a8294da63c521
nikm69 <53078776+nikm69@users.noreply.github.com>
2023-02-04 16:13:14 +0800

    Create alatbangunan.py

---
+++ alatbangunan.py
@@ -0,0 +1,30 @@
+print("DAFTAR ALAT BANGUNAN")
+#tangkapanlayar
+
+print()
+list_alat=[
+"gerinda",
+"bor mesin",
+"gergaji tangan",
+"cangkul",
+"sekop",
+"gerobak roda",
+"cetok bangunan",
+"helm safety",
+"pahat",
+"tangga lipat",
+"palu batu bata",
+"linggis",
+"pengaduk semen",
+"ukur roda",
+"penggaris",
+"tang",
+"obeng",
+"kapak",
+"crow bar",
+
+]
+for i, item in enumerate(list_alat):
+    print(f"{i+1}. {item}")
+
+
